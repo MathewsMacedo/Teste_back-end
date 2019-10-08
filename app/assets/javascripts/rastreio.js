d40f95ede2823f228862af738eed627a20d05950
Matheus Macedo <matheus.smacedo@hotmail.com>
2019-10-08 17:40:44 -0300

    Heroku rastreio

--- app/assets/javascripts/rastreio.js
+++ app/assets/javascripts/rastreio.js
@@ -66,5 +66,5 @@ function setPost(url, json) {
     
  document.addEventListener("turbolinks:load", function() {
 
-  setPost('http://localhost:3000/rastreios',getJson());
+  setPost('http://rastreios.herokuapp.com/rastreios',getJson());
 });
